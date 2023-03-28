cd55ad58a4dca9a5469efae24445e19df4c14d55
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-28 22:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // your code here
 
-document.getElementById("submit").addEventListener("click",(e)=>{
+let submit = document.getElementById("button");
+
+submit.addEventListener("click", (e)=>{
 	let h3 = document.getElementById("url");
-	h3.innerText = window.location.href;
+	h3.innerHTML = "hi";
+	console.log(window.location.href);
+	h3.innerHTML = window.location.href;
 })
