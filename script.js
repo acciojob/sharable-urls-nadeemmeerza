c81edb5ab3051bc7c2024dce8555c8db04f68ea9
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-28 22:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 let submit = document.getElementById("form");
 
-// document.getElementById("url").innerHTML = "name="+name+"&year="+year;
 submit.addEventListener('submit', (e)=>{
 	e.preventDefault();
 	let name = document.getElementById("name").value;
     let year = document.getElementById("year").value;
     document.getElementById("url").innerText="https://localhost:8080/"+"name="+name+"&year="+year;
-	
+	name.value='';
+    year.value='';
 })
