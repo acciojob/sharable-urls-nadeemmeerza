9a5d3dc3c708bf2d760d4373a549e146d0dbb12e
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-28 22:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let submit = document.getElementById("button");
 submit.addEventListener("click", (e)=>{
 	let h3 = document.getElementById("url");
 	h3.innerHTML = "hi";
-	console.log(window.location.href);
+	// console.log(window.location.href);
 	h3.innerHTML = window.location.href;
 })
